ac5b5def76e99cda83def1c432a2ad85b55e5c9e
tkdanr284 <tkdanr284@gmail.com>
2021-07-23 14:58:35 +0900

    boardlist1

---
+++ src/main/java/kh/spring/controller/BoardController.java
@@ -0,0 +1,17 @@
+package kh.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@RequestMapping("boardlist")
+	public String boardlist() {
+		
+		return "/board/boardlist";
+	}
+	
+
+}
